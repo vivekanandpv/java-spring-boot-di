0fa4c260989a8f57483579e7edc3bd44e7277177
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-13 12:23:44 +0530

    done

--- src/main/java/com/example/didemo/controllers/HomeController.java
+++ src/main/java/com/example/didemo/controllers/HomeController.java
@@ -1,8 +1,12 @@
 package com.example.didemo.controllers;
 
+import com.example.didemo.app.Car;
 import com.example.didemo.app.Vehicle;
+import com.example.didemo.config.AppConfig;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,12 +14,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1")
 
 public class HomeController {
-    @Autowired
-    private Vehicle vehicle;
 
     @GetMapping("/")
     public String getVehicle() {
-        return this.vehicle.drive();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Vehicle vehicle = context.getBean(Car.class);
+        return vehicle.drive();
     }
 
     @RequestMapping(value = "/items/{id}", method = RequestMethod.DELETE)
