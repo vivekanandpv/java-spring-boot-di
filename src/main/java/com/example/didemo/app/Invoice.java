31e3685f6f0770952603ae3f832391a8334f2acf
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-13 12:46:28 +0530

    done

--- src/main/java/com/example/didemo/app/Invoice.java
+++ src/main/java/com/example/didemo/app/Invoice.java
@@ -1,14 +1,22 @@
 package com.example.didemo.app;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class Invoice {
 
 
-    @Autowired
+
     private Customer customerInstance;
+
+    @Autowired
+    public void setCustomerInstance(Customer customerInstance) {
+
+        this.customerInstance = customerInstance;
+    }
 
     public String getName() {
         return "Invoice for " + customerInstance.getName();
